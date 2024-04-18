05dd5df3c0b02668582bf7e3714e1099c03d2431
LeoniLevi <leonilevi@gmail.com>
2024-04-18 11:35:26 +0300

    define two apps(instead one) in separate folders

--- app-sort/main.go
+++ app-sort/main.go
@@ -3,10 +3,8 @@ package main
 import (
 	//"sliceapp/sort"
 	"github.com/LeoniLevi/golab/sort"
-	"github.com/LeoniLevi/golab/tree"
 )
 
 func main() {
 	sort.ExamineSorting()
-	tree.TestIntTree()
 }
---
+++ app-tree/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/LeoniLevi/golab/tree"
+)
+
+func main() {
+	tree.TestIntTree()
+}
